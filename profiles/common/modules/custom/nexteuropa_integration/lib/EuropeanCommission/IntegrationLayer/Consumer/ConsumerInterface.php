1516e2f702f99852da93a8fc32ec7e57b87a4a3f
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-09 14:28:34 +0200

    NEXTEUROPA-4555: Add initial module code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/lib/EuropeanCommission/IntegrationLayer/Consumer/ConsumerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains EuropeanCommission\IntegrationLayer\Consumer\ConsumerInterface.
+ */
+
+namespace EuropeanCommission\IntegrationLayer\Consumer;
+
+/**
+ * Interface ConsumerInterface.
+ *
+ * @package EuropeanCommission\IntegrationLayer\Consumer
+ */
+interface ConsumerInterface {
+
+}
