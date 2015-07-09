9afe08609749b8474f257b30fd028137390e8853
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-09 14:28:34 +0200

    NEXTEUROPA-4555: Add initial module code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/lib/EuropeanCommission/IntegrationLayer/Backend/BackendInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains EuropeanCommission\IntegrationLayer\Backend\BackendInterface.
+ */
+
+namespace EuropeanCommission\IntegrationLayer\Backend;
+
+/**
+ * Interface BackendInterface.
+ *
+ * @package EuropeanCommission\IntegrationLayer\Backend
+ */
+interface BackendInterface {
+
+}
