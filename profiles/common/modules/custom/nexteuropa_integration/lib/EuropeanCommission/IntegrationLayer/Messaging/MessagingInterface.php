de787f646a3cb1a7d8981873a1f405e11a9d9fd2
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-09 14:28:34 +0200

    NEXTEUROPA-4555: Add initial module code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/lib/EuropeanCommission/IntegrationLayer/Messaging/MessagingInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains EuropeanCommission\IntegrationLayer\Messaging\MessagingInterface.
+ */
+
+namespace EuropeanCommission\IntegrationLayer\Messaging;
+
+/**
+ * Interface MessagingInterface.
+ *
+ * @package EuropeanCommission\IntegrationLayer\Messaging
+ */
+interface MessagingInterface {
+
+}
