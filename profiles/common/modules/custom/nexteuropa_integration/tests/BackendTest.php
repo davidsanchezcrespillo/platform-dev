9fa1b5c096031960aa45163184528e5ea0232298
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-09 14:28:34 +0200

    NEXTEUROPA-4555: Add initial module code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/tests/BackendTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains EuropeanCommission\IntegrationLayer\Tests\BackendTest.
+ */
+
+namespace EuropeanCommission\IntegrationLayer\Tests;
+
+/**
+ * Class BackendTest.
+ *
+ * @package EuropeanCommission\IntegrationLayer\Tests\BackendTest
+ */
+class BackendTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Smoke test.
+   */
+  public function testTrue() {
+    $this->assertTrue(TRUE);
+  }
+}
