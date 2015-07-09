2ca2a4d02f1a4bc15d7b88a85420b1b5a21ba373
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-09 14:28:34 +0200

    NEXTEUROPA-4555: Add initial module code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/lib/EuropeanCommission/IntegrationLayer/Producer/ProducerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains EuropeanCommission\IntegrationLayer\Producer\ProducerInterface.
+ */
+
+namespace EuropeanCommission\IntegrationLayer\Producer;
+
+/**
+ * Interface ProducerInterface.
+ *
+ * @package EuropeanCommission\IntegrationLayer\Producer
+ */
+interface ProducerInterface {
+
+}
