8b8a9d30abdf9a146a9eb135c8f4e813c3c10f15
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-16 14:49:42 +0200

    NEXTEUROPA-4555: Add initial class structure.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Messaging/MessagingInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\nexteuropa_integration\Messaging\MessagingInterface.
+ */
+
+namespace Drupal\nexteuropa_integration\Messaging;
+
+/**
+ * Interface MessagingInterface.
+ *
+ * @package Drupal\nexteuropa_integration\Messaging
+ */
+interface MessagingInterface {
+
+}
---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/FieldHandlers/DefaultFieldHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_integration\Producer\FieldHandlers\DefaultFieldHandler
+ */
+
+namespace Drupal\nexteuropa_integration\Producer\FieldHandlers;
+
+class DefaultFieldHandler {
+
+
+}
