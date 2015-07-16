591439694cdcab57a1eb53c9fbf9592cd33b96c7
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-16 17:57:45 +0200

    NEXTEUROPA-4555: Adding initial test calsses.

--- profiles/common/modules/custom/nexteuropa_integration/src/Producer/AbstractProducer.php
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/AbstractProducer.php
@@ -2,11 +2,13 @@
 
 /**
  * @file
- * Contains \Drupal\nexteuropa_integration\Producer\AbstractProducer
+ * Contains \Drupal\nexteuropa_integration\Producer\AbstractProducer.
  */
 
 namespace Drupal\nexteuropa_integration\Producer;
 
+use Drupal\nexteuropa_integration\Document\Formatter\FormatterInterface;
+use Drupal\nexteuropa_integration\DocumentInterface;
 use Drupal\nexteuropa_integration\Producer\FieldHandlers\FieldHandlerInterface;
 
 /**
@@ -14,12 +16,38 @@ use Drupal\nexteuropa_integration\Producer\FieldHandlers\FieldHandlerInterface;
  *
  * @package Drupal\nexteuropa_integration\Producer
  */
-class AbstractProducer implements ProducerInterface {
+abstract class AbstractProducer implements ProducerInterface {
 
+  /**
+   * Field handler instance.
+   *
+   * @var FieldHandlerInterface
+   */
+  private $fieldHandler = NULL;
 
-  function __construct($entity_type, $entity, FieldHandlerInterface $field_handler) {
+  /**
+   * Document handler instance.
+   *
+   * @var DocumentInterface
+   */
+  private $document = NULL;
+
+  /**
+   * Constructor.
+   *
+   * @param string $entity_type
+   *    Entity type.
+   * @param object $entity
+   *    Entity object.
+   * @param FieldHandlerInterface $field_handler
+   *    Field handler object.
+   * @param DocumentInterface $document
+   *    Document object.
+   * @param FormatterInterface $formatter
+   *    Formatter object.
+   */
+  public function __construct($entity_type, $entity, FieldHandlerInterface $field_handler, DocumentInterface $document, FormatterInterface $formatter) {
 
   }
-
 
 }
--- profiles/common/modules/custom/nexteuropa_integration/tests/ProducerTest.php
+++ profiles/common/modules/custom/nexteuropa_integration/tests/ProducerTest.php
@@ -20,4 +20,5 @@ class BackendTest extends \PHPUnit_Framework_TestCase {
   public function testTrue() {
     $this->assertTrue(TRUE);
   }
+
 }
