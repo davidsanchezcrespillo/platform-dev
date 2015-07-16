28a1c4a461b521043cff3641b164ab6d9ef6b9d3
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-16 17:55:55 +0200

    NEXTEUROPA-4555: Adding JsonFormatter class to Document component.

---
+++ profiles/common/modules/custom/nexteuropa_integration/tests/Document/Formatter/JsonFormatterTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_integration\Tests\Document\Formatter.
+ */
+
+namespace Drupal\nexteuropa_integration\Tests\Document\Formatter;
+
+use Drupal\nexteuropa_integration\Document;
+use Drupal\nexteuropa_integration\Document\Formatter\JsonFormatter;
+
+/**
+ * Class BackendTest.
+ *
+ * @package Drupal\nexteuropa_integration\Tests\BackendTest
+ */
+class JsonFormatterTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Test Json formatter.
+   */
+  public function testFormatter() {
+
+    $document = new Document();
+    $formatter = new JsonFormatter();
+    $this->assertEquals('{"_id":null,"default_language":"en","languages":["en"],"fields":[]}', $formatter->format($document));
+  }
+
+}
