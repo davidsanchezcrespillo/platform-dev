49220c7fb27c5afd65cdb3cb417f5449b993c01a
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-16 14:49:42 +0200

    NEXTEUROPA-4555: Add initial class structure.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/FieldHandlers/FieldHandlerInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ademarco
+ * Date: 7/16/15
+ * Time: 2:45 PM
+ */
+
+namespace Drupal\nexteuropa_integration\Producer\FieldHandlers;
+
+/**
+ * Interface FieldHandlerInterface.
+ *
+ * @package Drupal\nexteuropa_integration\Producer\FieldHandlers
+ */
+interface FieldHandlerInterface {
+
+}
