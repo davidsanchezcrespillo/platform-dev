29f94d9aeb41fbf863a2dfd647d412ece472b8f9
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-16 14:49:42 +0200

    NEXTEUROPA-4555: Add initial class structure.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Backend/BackendInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\nexteuropa_integration\Backend\BackendInterface.
+ */
+
+namespace Drupal\nexteuropa_integration\Backend;
+
+/**
+ * Interface BackendInterface.
+ *
+ * @package Drupal\nexteuropa_integration\Backend
+ */
+interface BackendInterface {
+
+}
---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/ProducerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\nexteuropa_integration\Producer\ProducerInterface.
+ */
+
+namespace Drupal\nexteuropa_integration\Producer;
+
+/**
+ * Interface ProducerInterface.
+ *
+ * @package Drupal\nexteuropa_integration\Producer
+ */
+interface ProducerInterface {
+
+}
