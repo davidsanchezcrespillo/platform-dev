9406bdf9fe11f936531873df8b281e033fb4036a
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-17 11:12:42 +0200

    NEXTEUROPA-4548: Adding producer-related code.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/DefaultProducer.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\nexteuropa_integration\Producer\DefaultProducer.
+ */
+
+namespace Drupal\nexteuropa_integration\Producer;
+
+use Drupal\nexteuropa_integration\Document\Formatter\FormatterInterface;
+use Drupal\nexteuropa_integration\DocumentInterface;
+use Drupal\nexteuropa_integration\Producer\FieldHandlers\FieldHandlerInterface;
+
+/**
+ * Class DefaultProducer.
+ *
+ * @package Drupal\nexteuropa_integration\Producer
+ */
+class DefaultProducer extends AbstractProducer {
+
+}
