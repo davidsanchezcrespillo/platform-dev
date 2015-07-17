03b36b23415760d86d44f6348dcf8bdab90f02e2
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-17 17:18:31 +0200

    NEXTEUROPA-4549: Clean up module and api files.

--- profiles/common/modules/custom/nexteuropa_integration/nexteuropa_integration.api.php
+++ profiles/common/modules/custom/nexteuropa_integration/nexteuropa_integration.api.php
@@ -21,3 +21,10 @@ function hook_nexteuropa_integration_producer_info() {
 function hook_nexteuropa_integration_producer_info_alter(&$producers) {
   $producers['node']['class'] = '\Drupal\custom\Producer\CustomProducer';
 }
+
+/**
+ * Implements hook_nexteuropa_integration_producer_document_build_alter().
+ */
+function hook_nexteuropa_integration_producer_document_build_alter(\Drupal\nexteuropa_integration\DocumentInterface &$document) {
+  $document->setMetadata('tag', 'My custom tag');
+}
