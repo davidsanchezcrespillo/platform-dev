dd30a4bb2bc3f29502e2b7300c070483a3c948f1
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-17 17:20:44 +0200

    NEXTEUROPA-4549: Adding actual producer classes.

--- profiles/common/modules/custom/nexteuropa_integration/src/Producer/NodeProducer.php
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/NodeProducer.php
@@ -25,7 +25,41 @@ class NodeProducer extends AbstractProducer {
    *    Built document object.
    */
   public function build() {
-    return new Document();
+
+    // Set document metadata.
+    $properties = array(
+      'type',
+      'created',
+      'changed',
+    );
+    foreach ($properties as $name) {
+      $this->getDocument()->setMetadata($name, $this->getEntityWrapper()->getProperty($name));
+    }
+
+    // Set multilingual-related metadata.
+    $this->getDocument()->setMetadata('languages', $this->getEntityWrapper()->getAvailableLanguages());
+    $this->getDocument()->setMetadata('default_language', $this->getEntityWrapper()->getDefaultLanguage());
+
+    // Set field values.
+    // @todo: improve this part by using field handlers, name altering, etc.
+    foreach ($this->getEntityWrapper()->getAvailableLanguages() as $language) {
+      $this->getDocument()->setCurrentLanguage($language);
+      foreach ($this->getEntityWrapper()->getFieldList() as $field_name) {
+        $value = $this->getEntityWrapper()->getField($field_name, $language);
+        if (is_array($value)) {
+          foreach ($value as $column_name => $column_value) {
+            $this->getDocument()->setField($field_name . '_' . $column_name, $column_value);
+          }
+        }
+        else {
+          $this->getDocument()->setField($field_name, $value);
+        }
+      }
+    }
+
+    $document = $this->getDocument();
+    drupal_alter('nexteuropa_integration_producer_document_build', $document);
+    return $document;
   }
 
 }
