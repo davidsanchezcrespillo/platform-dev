8bf4f7c134bb92efa03c4f3c87d6085de0f77ff5
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-18 18:30:49 +0200

    NEXTEUROPA-4549: Improve producer inheritance structure.

--- profiles/common/modules/custom/nexteuropa_integration/src/Producer/NodeProducer.php
+++ profiles/common/modules/custom/nexteuropa_integration/src/Producer/NodeProducer.php
@@ -7,12 +7,6 @@
 
 namespace Drupal\nexteuropa_integration\Producer;
 
-use Drupal\nexteuropa_integration\Document;
-use Drupal\nexteuropa_integration\Document\Formatter\FormatterInterface;
-use Drupal\nexteuropa_integration\DocumentInterface;
-use Drupal\nexteuropa_integration\Producer\FieldHandlers\DefaultFieldHandler;
-use Drupal\nexteuropa_integration\Producer\FieldHandlers\FieldHandlerInterface;
-
 /**
  * Class NodeProducer.
  *
@@ -21,52 +15,31 @@ use Drupal\nexteuropa_integration\Producer\FieldHandlers\FieldHandlerInterface;
 class NodeProducer extends AbstractProducer {
 
   /**
-   * Build document object using the entity the producer was instantiated with.
-   *
-   * @return DocumentInterface
-   *    Built document object.
+   * {@inheritdoc}
    */
-  public function build() {
-
-    // Set document metadata.
-    $properties = array(
-      'type',
-      'created',
-      'changed',
-    );
-    foreach ($properties as $name) {
-      $this->getDocument()->setMetadata($name, $this->getEntityWrapper()->getProperty($name));
-    }
-
-    // Set multilingual-related metadata.
-    $this->getDocument()->setMetadata('languages', $this->getEntityWrapper()->getAvailableLanguages());
-    $this->getDocument()->setMetadata('default_language', $this->getEntityWrapper()->getDefaultLanguage());
-
-    // Set field values.
-    foreach ($this->getEntityWrapper()->getAvailableLanguages() as $language) {
-      foreach ($this->getEntityWrapper()->getFieldList() as $field_name) {
-        $this->getFieldHandler($field_name, $language)->process();
-      }
-    }
-    $document = $this->getDocument();
-    drupal_alter('nexteuropa_integration_producer_document_build', $document);
-    return $document;
+  public function getProducerContentId() {
+    return 'node-' . $this->getEntityWrapper()->getBundle() . '-' . $this->getEntityWrapper()->nid->value();
   }
 
   /**
-   * Returns field handler interface.
-   *
-   * @param string $field_name
-   *    Current field name.
-   * @param string $language
-   *    Current field language code.
-   *
-   * @return FieldHandlerInterface
-   *    Field handler instance.
+   * {@inheritdoc}
    */
-  protected function getFieldHandler($field_name, $language) {
-    // @todo: get field handler by type, atm only returns default.
-    return new DefaultFieldHandler($field_name, $language, $this->getEntityWrapper(), $this->getDocument());
+  public function getDocumentType() {
+    return $this->getEntityWrapper()->getProperty('type');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDocumentCreationDate() {
+    return $this->getEntityWrapper()->getProperty('created');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDocumentUpdateDate() {
+    return $this->getEntityWrapper()->getProperty('changed');
   }
 
 }
