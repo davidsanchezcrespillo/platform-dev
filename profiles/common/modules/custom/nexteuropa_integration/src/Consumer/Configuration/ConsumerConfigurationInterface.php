e3592646ecbb36ecd0484fb6a91b9da1e41fa530
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-20 19:47:42 +0200

    NEXTEUROPA-4548: Started working on consumer objects.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Consumer/Configuration/ConsumerConfigurationInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains ConsumerConfigurationInterface.
+ */
+
+namespace Drupal\nexteuropa_integration\Consumer\Configuration;
+
+/**
+ * Interface ConsumerInterface.
+ *
+ * @package Drupal\nexteuropa_integration\Consumer
+ */
+interface ConsumerConfigurationInterface {
+
+
+}
