e658ce352e8db06ccd1ff260450e2a4578f8d1cf
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-21 20:47:59 +0200

    NEXTEUROPA-4548: Adding rest backend tests, only for sprint review demo.

--- profiles/common/modules/custom/nexteuropa_integration/src/Backend/RestBackend.php
+++ profiles/common/modules/custom/nexteuropa_integration/src/Backend/RestBackend.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\nexteuropa_integration\Backend;
 
+use Drupal\nexteuropa_integration\Document\Document;
 use Drupal\nexteuropa_integration\Document\DocumentInterface;
 
 /**
@@ -33,7 +34,7 @@ class RestBackend extends AbstractBackend {
   public function create(DocumentInterface $document) {
     $options = array();
     $options['method'] = 'POST';
-    $options['data'] = $document->getDocument();
+    $options['data'] = $this->getFormatter()->format($document);
     $response = drupal_http_request($this->getUri(), $options);
     return $response;
   }
@@ -44,8 +45,15 @@ class RestBackend extends AbstractBackend {
   public function read(DocumentInterface $document) {
     $options = array();
     $options['method'] = 'GET';
-    $response = drupal_http_request($this->getUri() . '/' . $document->getId(), $options);
-    return $response;
+    $response = drupal_http_request($this->getUri() . '/' . $this->getBackendId($document), $options);
+
+    if ($response->code == 200) {
+      $data = json_decode($response->data);
+      return new Document($data);
+    }
+    else {
+      return NULL;
+    }
   }
 
   /**
@@ -54,9 +62,15 @@ class RestBackend extends AbstractBackend {
   public function update(DocumentInterface $document) {
     $options = array();
     $options['method'] = 'PUT';
-    $options['data'] = $document->getDocument();
-    $response = drupal_http_request($this->getUri(), $options);
-    return $response;
+    $options['data'] = $this->getFormatter()->format($document);
+    $response = drupal_http_request($this->getUri() . '/' . $this->getBackendId($document), $options);
+
+    if ($response->code == 200) {
+      return $this->read($document);
+    }
+    else {
+      return NULL;
+    }
   }
 
   /**
@@ -65,16 +79,37 @@ class RestBackend extends AbstractBackend {
   public function delete(DocumentInterface $document) {
     $options = array();
     $options['method'] = 'DELETE';
-    $response = drupal_http_request($this->getUri() . '/' . $document->getId(), $options);
-    return $response;
+    $response = drupal_http_request($this->getUri() . '/' . $this->getBackendId($document), $options);
+
+    if ($response->code == 200) {
+      // @todo: backend sets deleted_by_producer = true.
+      return TRUE;
+    }
+    else {
+      return NULL;
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function getBackendId(DocumentInterface $document) {
-    // @todo: ask service for remote id.
-    return $document->getMetadata('producer_content_id');
+    $options = array();
+    $options['method'] = 'GET';
+    $producer = $document->getMetadata('producer');
+    $producer_content_id = $document->getMetadata('producer_content_id');
+    if (!$producer || !$producer_content_id) {
+      return NULL;
+    }
+    $response = drupal_http_request($this->getBase() . '/uuid/' . $producer . '/' . $producer_content_id, $options);
+
+    if ($response->code == 200) {
+      $data = json_decode($response->data);
+      return $data->rows[0]->id;
+    }
+    else {
+      return NULL;
+    }
   }
 
 }
---
+++ profiles/common/modules/custom/nexteuropa_integration/tests/fixtures/configuration/producer-local.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Configuration object export.
+ */
+
+$export = new \stdClass();
+$export->label = 'Local producer';
+$export->name = 'userProducer';
+$export->password = 'pass';
