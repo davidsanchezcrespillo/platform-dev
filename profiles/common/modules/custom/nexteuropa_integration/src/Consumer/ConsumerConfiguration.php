e2664338ba2eada89b45994b5df310ebf78afcd0
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-23 16:34:27 +0200

    NEXTEUROPA-4548: Remove occurrences of obsolete configuration class in consumer component.

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Consumer/ConsumerConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains ConsumerConfiguration.
+ */
+
+namespace Drupal\nexteuropa_integration\Consumer;
+
+use Drupal\nexteuropa_integration\Configuration\AbstractConfiguration;
+
+/**
+ * Class ConsumerConfiguration.
+ *
+ * @package Drupal\nexteuropa_integration\Consumer
+ */
+class ConsumerConfiguration extends AbstractConfiguration {
+
+}
