08d09a0aa8a1f06c751ef8791c69f43d04ddf1c3
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-23 16:32:41 +0200

    NEXTEUROPA-4548: Refactoring configuration as entity configuration, so to have them exportable out of the box.

---
+++ profiles/common/modules/custom/nexteuropa_integration/tests/Backend/ConfigurationTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @file
+ * Contains ConfigurationTest.
+ */
+
+namespace Drupal\nexteuropa_integration\Tests\Backend;
+
+use Drupal\nexteuropa_integration\Backend\Configuration\BackendConfiguration;
+use Drupal\nexteuropa_integration\Configuration\ConfigurationFactory;
+use Drupal\nexteuropa_integration\Tests\AbstractTest;
+
+/**
+ * Class ConfigurationTest.
+ *
+ * @package Drupal\nexteuropa_integration\Tests\Backend
+ */
+class ConfigurationTest extends AbstractTest {
+
+  /**
+   * Test configuration entity CRUD operations.
+   *
+   * @dataProvider configurationProvider
+   */
+  public function testConfigurationEntityCrud($data) {
+
+    /** @var BackendConfiguration $configuration */
+    $configuration = entity_create('integration_backend', (array) $data);
+    $configuration->save();
+
+    $reflection = new \ReflectionClass($configuration);
+    $this->assertEquals('Drupal\nexteuropa_integration\Backend\Configuration\BackendConfiguration', $reflection->getName());
+
+    $this->assertEquals($data->machine_name, $configuration->identifier());
+    $this->assertEquals(ENTITY_CUSTOM, $configuration->getStatus());
+    $this->assertEquals($data->options['endpoint'], $configuration->getEndpoint());
+    $this->assertEquals($data->options['base_path'], $configuration->getBasePath());
+
+    $machine_name = $configuration->identifier();
+    $this->assertNotNull(ConfigurationFactory::load('integration_backend', $machine_name));
+
+    $configuration->delete();
+    $this->assertFalse(ConfigurationFactory::load('integration_backend', $machine_name));
+  }
+
+  /**
+   * Test export entity.
+   *
+   * @dataProvider configurationProvider
+   */
+  public function testExportImport($data) {
+
+    /** @var BackendConfiguration $configuration */
+    $configuration = entity_create('integration_backend', (array) $data);
+
+    $json = entity_export('integration_backend', $configuration);
+    $decoded = json_decode($json);
+    $this->assertNotNull($decoded);
+    $this->assertEquals($data->machine_name, $decoded->machine_name);
+
+    $entity = entity_import('integration_backend', $json);
+    $this->assertEquals($data->machine_name, $entity->identifier());
+    $this->assertEquals($data->options['endpoint'], $entity->getEndpoint());
+    $this->assertEquals($data->options['base_path'], $entity->getBasePath());
+  }
+
+  /**
+   * Data provider.
+   *
+   * @return array
+   */
+  public function configurationProvider() {
+
+    return array(
+      array($this->getConfigurationFixture('backend', 'test_configuration')),
+    );
+  }
+}
