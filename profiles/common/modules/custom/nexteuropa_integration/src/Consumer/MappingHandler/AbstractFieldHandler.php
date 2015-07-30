1689437b469a721a5361aedfd6255e230b145e6e
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-30 15:30:43 +0200

    NEXTEUROPA-4548: Adding hook_nexteuropa_integration_consumer_mapping_handler_info().

---
+++ profiles/common/modules/custom/nexteuropa_integration/src/Consumer/MappingHandler/AbstractFieldHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains AbstractMappingHandler.
+ */
+
+namespace Drupal\nexteuropa_integration\Consumer\MappingHandler;
+
+use Drupal\nexteuropa_integration\Consumer\Consumer;
+
+/**
+ * Class AbstractMappingHandler.
+ *
+ * @package Drupal\nexteuropa_integration\Consumer
+ */
+abstract class AbstractMappingHandler implements MappingHandlerInterface {
+
+  /**
+   * Current consumer object
+   *
+   * @var Consumer
+   */
+  protected $consumer = NULL;
+
+  /**
+   * Constructor.
+   *
+   * @param Consumer $consumer
+   *    Consumer object.
+   */
+  public function __construct(Consumer $consumer) {
+    $this->consumer = $consumer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConsumer() {
+    return $this->consumer;
+  }
+
+}
