bdebae383332d750131b150339af7e66308343e9
DE MARCO Antonio <antonio.demarco@engib.be>
2015-07-30 17:23:08 +0200

    NEXTEUROPA-4548: Add ProducerFactory class along with tests.

---
+++ profiles/common/modules/custom/nexteuropa_integration/tests/Producer/ProducerFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains ProducerFactoryTest.
+ */
+
+namespace Drupal\nexteuropa_integration\Tests\Producer;
+
+use Drupal\nexteuropa_integration\Producer\ProducerFactory;
+use Drupal\nexteuropa_integration\Tests\AbstractTest;
+
+/**
+ * Class ProducerFactoryTest.
+ *
+ * @package Drupal\nexteuropa_integration\Tests\Producer
+ */
+class ProducerFactoryTest extends AbstractTest {
+
+  /**
+   * Test create method.
+   */
+  public function testFactory() {
+    $node = $this->getExportedEntityFixture('node', 'integration_test', 1);
+    $producer_info = nexteuropa_integration_producer_get_producer_info();
+    $producer_class = $producer_info[$this->producer_configuration->getType()]['class'];
+
+    $producer = ProducerFactory::getInstance('test_configuration', $node);
+
+    $reflection = new \ReflectionClass($producer);
+    $this->assertEquals($producer_class, $reflection->getName());
+  }
+
+}
