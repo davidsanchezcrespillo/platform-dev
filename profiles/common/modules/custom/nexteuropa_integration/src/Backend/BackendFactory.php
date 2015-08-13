a0709aa72cc273e2b78f931cf41e7dd9085b196e
DE MARCO Antonio <antonio.demarco@engib.be>
2015-08-13 12:16:17 +0200

    NEXTEUROPA-4548: Adds static cache to backend factory.

--- profiles/common/modules/custom/nexteuropa_integration/src/Backend/BackendFactory.php
+++ profiles/common/modules/custom/nexteuropa_integration/src/Backend/BackendFactory.php
@@ -19,32 +19,44 @@ use Drupal\nexteuropa_integration\Backend\Configuration\BackendConfiguration;
 class BackendFactory {
 
   /**
+   * Keeps instances of already build backend objects.
+   *
+   * @var array[AbstractBackend]
+   */
+  static private $instances = array();
+
+  /**
    * Instantiate and return a backend object given its configuration.
    *
    * @param string $machine_name
    *    Backend configuration machine name.
+   * @param bool|FALSE $reset
+   *    Whereas to force a new instance of a specific backend object.
    *
    * @return AbstractBackend
    *    Backend instance.
    */
-  static public function getInstance($machine_name) {
-    /** @var BackendConfiguration $configuration */
-    $configuration = self::loadConfiguration($machine_name);
+  static public function getInstance($machine_name, $reset = FALSE) {
+    if (!isset(self::$instances[$machine_name]) || $reset) {
+      /** @var BackendConfiguration $configuration */
+      $configuration = self::loadConfiguration($machine_name);
 
-    $backend_info = nexteuropa_integration_backend_get_backend_info();
-    $response_info = nexteuropa_integration_backend_get_response_handler_info();
-    $formatter_info = nexteuropa_integration_backend_get_formatter_handler_info();
+      $backend_info = nexteuropa_integration_backend_get_backend_info();
+      $response_info = nexteuropa_integration_backend_get_response_handler_info();
+      $formatter_info = nexteuropa_integration_backend_get_formatter_handler_info();
 
-    $backend_class = $backend_info[$configuration->getType()]['class'];
-    $response_class = $response_info[$configuration->getResponse()]['class'];
-    $formatter_class = $formatter_info[$configuration->getFormatter()]['class'];
+      $backend_class = $backend_info[$configuration->getType()]['class'];
+      $response_class = $response_info[$configuration->getResponse()]['class'];
+      $formatter_class = $formatter_info[$configuration->getFormatter()]['class'];
 
-    foreach (array($backend_class, $response_class, $formatter_class) as $class) {
-      if (!class_exists($class)) {
-        throw new \InvalidArgumentException("Class $class does not exists");
+      foreach (array($backend_class, $response_class, $formatter_class) as $class) {
+        if (!class_exists($class)) {
+          throw new \InvalidArgumentException("Class $class does not exists");
+        }
       }
+      self::$instances[$machine_name] = new $backend_class($configuration, new $response_class(), new $formatter_class());
     }
-    return new $backend_class($configuration, new $response_class(), new $formatter_class());
+    return self::$instances[$machine_name];
   }
 
   /**
