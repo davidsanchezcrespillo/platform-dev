cfc557e466121cb0120ab5c2b30eece336608067
DE MARCO Antonio <antonio.demarco@engib.be>
2015-08-13 10:53:02 +0200

    NEXTEUROPA-4548: Refactor producer and integration tests.

--- profiles/common/modules/custom/nexteuropa_integration/tests/IntegrationTest.php
+++ profiles/common/modules/custom/nexteuropa_integration/tests/IntegrationTest.php
@@ -23,73 +23,25 @@ class IntegrationTest extends AbstractTest {
 
   /**
    * Test producer-consumer workflow.
-   *
-   * @param string $bundle
-   *    Node bundle.
-   * @param int $id
-   *    Node ID.
-   *
-   * @dataProvider nodeFixturesProvider
    */
   public function testProducerConsumerWorkflow($bundle, $id) {
-    $node = $this->getExportedEntityFixture('node', $bundle, $id);
-
     // Get backend, producer and consumer instances.
     $backend = BackendFactory::getInstance('test_configuration');
-    $producer = ProducerFactory::getInstance('test_configuration', $node);
     $consumer = Consumer::getInstance('test_configuration');
 
-    // Build document: at this point it should not have a remote ID.
-    $document = $producer->build();
-    $this->assertNull($document->getId());
+    foreach ($this->nodeFixturesDataProvider() as $row) {
+      $node = $this->getExportedEntityFixture('node', $row[0], $row[1]);
 
-    // Each backend is responsible for fetching a document's remote ID.
-    $this->assertEquals($this->expectedDocumentId($node), $backend->getBackendId($document));
+      // Build document: at this point it should not have a remote ID.
+      $document = ProducerFactory::getInstance('test_configuration', $node)->build();
+      $document = $backend->create($document);
 
-    // Test backend create method.
-    $document = $backend->create($document);
+      // Test that backend create does assign an ID to a document.
+      $this->assertEquals($this->expectedNodeDocumentId($node), $document->getId());
+    }
 
-    // Test that backend create does assign an ID to a document.
-    $this->assertEquals($this->expectedDocumentId($node), $document->getId());
 
-    // Test backend read method.
-    $document = $backend->read($document);
-
-    // Test backend update method.
-    $document->setCurrentLanguage('en')->setField('title_field', 'English title updated');
-    $updated_document = $backend->update($document);
-    $this->assertEquals('English title updated', $updated_document->getFieldValue('title_field'));
-
-    // Test backend delete method.
-    $backend->delete($updated_document);
-    $this->assertFalse($backend->read($updated_document));
-  }
-
-  /**
-   * Return expected document ID.
-   *
-   * @param object $node
-   *    Node object.
-   *
-   * @return string
-   *    Expected document ID.
-   */
-  protected function expectedDocumentId($node) {
-    return 'node-integration-test-' . $node->nid;
-  }
-
-  /**
-   * Node fixture data provider.
-   *
-   * @return array
-   *    List of fixtures types and IDs.
-   */
-  public function nodeFixturesProvider() {
-    return array(
-      array('integration_test', 1),
-      array('integration_test', 2),
-      array('integration_test', 3),
-    );
+    return;
   }
 
 }
