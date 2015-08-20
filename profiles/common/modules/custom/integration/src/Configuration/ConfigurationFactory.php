e57c5360769bb143ad23c8513a113c11be870f0f
DE MARCO Antonio <antonio.demarco@engib.be>
2015-08-20 11:02:44 +0200

    NEXTEUROPA-4548: Rename MemoryResponse into RawResponse.

--- profiles/common/modules/custom/integration/src/Configuration/ConfigurationFactory.php
+++ profiles/common/modules/custom/integration/src/Configuration/ConfigurationFactory.php
@@ -26,14 +26,17 @@ class ConfigurationFactory {
    * @param string $machine_name
    *    Configuration entity machine name.
    *
-   * @return AbstractConfiguration|false
+   * @return AbstractConfiguration
    *    Loaded configuration entity.
    */
   public static function load($type, $machine_name) {
     if ($configuration = entity_load_single($type, $machine_name)) {
       return $configuration;
     }
-    return FALSE;
+    else {
+      $args = array('@machine_name' => $machine_name, '@type' => $type);
+      throw new \InvalidArgumentException(t('Configuration entity "@machine_name" of type "@type" not found.', $args));
+    }
   }
 
 }
