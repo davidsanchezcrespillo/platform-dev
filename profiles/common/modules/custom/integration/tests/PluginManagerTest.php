8827088dc101c5a833dd5a9efbfb5a02cab5baeb
DE MARCO Antonio <antonio.demarco@engib.be>
2015-08-20 12:23:01 +0200

    NEXTEUROPA-4548: Adds utility methods to plugin manager class.

--- profiles/common/modules/custom/integration/tests/PluginManagerTest.php
+++ profiles/common/modules/custom/integration/tests/PluginManagerTest.php
@@ -26,4 +26,25 @@ class PluginManagerTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(array('field_handler'), PluginManager::getInstance('producer')->getComponents());
   }
 
+  /**
+   * Test info hooks.
+   */
+  public function testInfo() {
+
+    $info = PluginManager::getInstance('consumer')->setComponent('mapping_handler')->getInfo();
+    $expected = array(
+      'title_mapping',
+      'file_field_mapping',
+      'text_with_summary_mapping',
+    );
+    $this->assertEquals($expected, array_keys($info));
+
+    $manager = PluginManager::getInstance('backend')->setComponent('response_handler');
+
+    $label = $manager->getLabel();
+    $class = $manager->getClass();
+    $description = $manager->getDescription();
+
+    return;
+  }
 }
